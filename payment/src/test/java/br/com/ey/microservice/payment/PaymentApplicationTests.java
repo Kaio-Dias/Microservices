56f2ac23ba69789b9d2083434b604de8b36e28a0
9Cloud6 <86304172+9Cloud6@users.noreply.github.com>
2023-11-20 21:50:52 -0300

    Primeiro Commit

---
+++ payment/src/test/java/br/com/ey/microservice/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ey.microservice.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
